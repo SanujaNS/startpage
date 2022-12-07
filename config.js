8e3696d7d287cf81e5aaa0419846553240cf58c6
Sanuja Seneviratne <66342986+SanujaNS@users.noreply.github.com>
2022-12-07 21:33:36 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,230 @@
+// ╔╗ ╔═╗╔╗╔╔╦╗╔═╗
+// ╠╩╗║╣ ║║║ ║ ║ ║
+// ╚═╝╚═╝╝╚╝ ╩ ╚═╝
+// ┌─┐┌─┐┌┐┌┌─┐┬┌─┐┬ ┬┬─┐┌─┐┌┬┐┬┌─┐┌┐┌
+// │  │ ││││├┤ ││ ┬│ │├┬┘├─┤ │ ││ ││││
+// └─┘└─┘┘└┘└  ┴└─┘└─┘┴└─┴ ┴ ┴ ┴└─┘┘└┘
+
+const CONFIG = {
+	// ┌┐ ┌─┐┌─┐┬┌─┐┌─┐
+	// ├┴┐├─┤└─┐││  └─┐
+	// └─┘┴ ┴└─┘┴└─┘└─┘
+
+	// General
+	name: 'Sanuja',
+	imageBackground: true,
+	openInNewTab: false,
+	twelveHourFormat: true,
+
+	// Greetings
+	greetingMorning: 'Good morning!',
+	greetingAfternoon: 'Good afternoon,',
+	greetingEvening: 'Good evening,',
+	greetingNight: 'Go to Sleep!',
+
+	// Layout
+	bentoLayout: 'buttons', // 'bento', 'lists', 'buttons'
+
+	// Weather
+	weatherKey: 'bc288a570a776da1e4d08ce987ab0a31', // Write here your API Key
+	weatherIcons: 'OneDark', // 'Onedark', 'Nord', 'Dark', 'White'
+	weatherUnit: 'C', // 'F', 'C'
+	language: 'en', // More languages in https://openweathermap.org/current#multi
+
+	trackLocation: false, // If false or an error occurs, the app will use the lat/lon below
+	defaultLatitude: '7.309',
+	defaultLongitude: '80.710',
+
+	// Autochange
+	autoChangeTheme: false,
+
+	// Autochange by OS
+	changeThemeByOS: false,
+
+	// Autochange by hour options (24hrs format, string must be in: hh:mm)
+	changeThemeByHour: false,
+	hourDarkThemeActive: '18:30',
+	hourDarkThemeInactive: '07:00',
+
+	// ┌┐ ┬ ┬┌┬┐┌┬┐┌─┐┌┐┌┌─┐
+	// ├┴┐│ │ │  │ │ ││││└─┐
+	// └─┘└─┘ ┴  ┴ └─┘┘└┘└─┘
+
+	firstButtonsContainer: [
+		{
+			id: '1',
+			name: 'Gateway',
+			icon: 'globe',
+			link: 'https://sanuja.biz/',
+		},
+		{
+			id: '2',
+			name: 'Blog',
+			icon: 'scroll',
+			link: 'https://sanujas.com/',
+		},
+		{
+			id: '3',
+			name: 'YT-Local',
+			icon: 'youtube',
+			link: 'https://yt.sanuja.biz/',
+		},
+		{
+			id: '4',
+			name: 'GDrive Index',
+			icon: 'hard-drive',
+			link: 'https://sanuja.gq/0:/',
+		},
+		{
+			id: '5',
+			name: 'SanujaPut-Music',
+			icon: 'music',
+			link: 'https://sanujasvps.tk/',
+		},
+		{
+			id: '6',
+			name: 'Notion',
+			icon: 'sticky-note',
+			link: 'https://www.notion.so/',
+		},
+	],
+
+	secondButtonsContainer: [
+		{
+			id: '1',
+			name: 'Github',
+			icon: 'github',
+			link: 'https://github.com/SanujaNS',
+		},
+		{
+			id: '2',
+			name: 'Sanuja Main',
+			icon: 'mail',
+			link: 'https://mail.google.com/mail/u/0/#inbox',
+		},
+		{
+			id: '3',
+			name: 'Team-SanujaPut',
+			icon: 'folder-open',
+			link: 'https://drive.google.com/drive/u/0/folders/0AL2OF39HIBTfUk9PVA',
+		},
+		{
+			id: '4',
+			name: 'Main-SanujaPut',
+			icon: 'folder-closed',
+			link: 'https://drive.google.com/drive/u/1/my-drive',
+		},
+		{
+			id: '5',
+			name: 'SanujaPut',
+			icon: 'mail',
+			link: 'https://mail.google.com/mail/u/1/#inbox',
+		},
+		{
+			id: '6',
+			name: 'Nextcloud',
+			icon: 'server',
+			link: 'https://sanuja.blggr.one/',
+		},
+	],
+
+	// ┬  ┬┌─┐┌┬┐┌─┐
+	// │  │└─┐ │ └─┐
+	// ┴─┘┴└─┘ ┴ └─┘
+
+	// First Links Container
+	firstlistsContainer: [
+		{
+			icon: 'music',
+			id: '1',
+			links: [
+				{
+					name: 'Inspirational',
+					link: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
+				},
+				{
+					name: 'Classic',
+					link: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
+				},
+				{
+					name: 'Oldies',
+					link: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
+				},
+				{
+					name: 'Rock',
+					link: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
+				},
+			],
+		},
+		{
+			icon: 'coffee',
+			id: '2',
+			links: [
+				{
+					name: 'Linkedin',
+					link: 'https://www.linkedin.com',
+				},
+				{
+					name: 'Dribbble',
+					link: 'https://www.dribbble.com',
+				},
+				{
+					name: 'Trello',
+					link: 'https://www.trello.com',
+				},
+				{
+					name: 'Slack',
+					link: 'https://www.slack.com',
+				},
+			],
+		},
+	],
+
+	// Second Links Container
+	secondListsContainer: [
+		{
+			icon: 'binary',
+			id: '1',
+			links: [
+				{
+					name: 'Spotify',
+					link: 'https://www.spotify.com',
+				},
+				{
+					name: 'Reddit',
+					link: 'https://www.reddit.com',
+				},
+				{
+					name: 'Hashnode',
+					link: 'https://www.hashnode.com',
+				},
+				{
+					name: 'Pocket',
+					link: 'https://www.pocket.com',
+				},
+			],
+		},
+		{
+			icon: 'github',
+			id: '2',
+			links: [
+				{
+					name: 'Front',
+					link: 'https://www.reddit.com/r/Frontend/',
+				},
+				{
+					name: 'Rust',
+					link: 'https://www.reddit.com/r/rust/',
+				},
+				{
+					name: 'Go',
+					link: 'https://www.reddit.com/r/golang/',
+				},
+				{
+					name: 'Repos',
+					link: 'https://github.com/migueravila',
+				},
+			],
+		},
+	],
+};
